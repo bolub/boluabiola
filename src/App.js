1c411ab773aa408a3bc96c7e0035877d50b256b0
bolub <abiol5202@gmail.com>
2019-06-26 16:23:40 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="btn btn-primary">CLick</div>
+      </div>
+    );
+  }
+}
+
+export default App;
